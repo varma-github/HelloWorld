023b9ba434aa3fcca73c49bce2b1813e6bbf27b8
varma-github <varmaislearningjava@gmail.com>
2017-01-12 00:32:19 -0600

    Initial commit

---
+++ src/com/learning/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.learning;
+
+/**
+ * Created by simmalra on 1/12/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+
+}
